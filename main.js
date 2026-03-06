7875cbf404bc8feb07fe86bc1968089cd2ef2904
PonomarenkoYaroslav <ponomarenko.yaroslav1@lll.kpi.ua>
2026-03-06 18:07:31 +0100

    basic comparator implementation

--- main.js
+++ main.js
@@ -1,14 +1,45 @@
-
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const path = require('path');
 
 const { FileTree } = require('./fileTree');
+const { compareTrees } = require('./sync');
 
-(async () => {
-    const treeA = new FileTree('C:/folderA');
-    const treeB = new FileTree('C:/folderB');
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 900,
+        height: 700,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+ipcMain.handle('select-folder', async () => {
+    const result = await dialog.showOpenDialog({
+        properties: ['openDirectory']
+    });
+
+    if (result.canceled) return null;
+    return result.filePaths[0];
+});
+
+ipcMain.handle('scan-folders', async (event, folderA, folderB) => {
+
+    const treeA = new FileTree(folderA);
+    const treeB = new FileTree(folderB);
 
     await treeA.build();
     await treeB.build();
 
-    console.log('Tree A files:', treeA.pathMap.size);
-    console.log('Tree B files:', treeB.pathMap.size);
-})();
+    const result = compareTrees(treeA, treeB);
+
+    return {
+        filesA: treeA.pathMap.size,
+        filesB: treeB.pathMap.size,
+        identical: result.identical
+    };
+});
